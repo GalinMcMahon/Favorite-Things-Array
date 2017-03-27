98c068bd2b1eba38b973a638441d344971a4ccf5
Galin McMahon and Tyler Stephenson <galin+tyler@>
2017-03-27 10:33:18 -0700

    create HTML and js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,27 @@
 $(function(){
+  // $(document).keypress(function (e) {
+  //   if (e.which == 13 || event.keyCode == 13) {
+  //     var favoriteThings = [];
+  //     // var yourFavs = $("#yourFavs").val();
+  //     favoriteThings.push($("#yourFavs").val());
+  //     console.log(favoriteThings);
+  //   }
+  // });
 
+  $("#submit").click(function() {
+    // var yourFavs = $("#yourFavs").val();
+    var favoriteThings = [];
 
-  
+    favoriteThings.push($("#yourFavs").val());
+    favoriteThings.push($("#yourFavs1").val());
+    favoriteThings.push($("#yourFavs2").val());
+
+    for (i = 0; i < favoriteThings.length; i++) {
+      $("#result-list").append("<li>" + favoriteThings[i] + "</li>");
+    }
+
+    console.log(favoriteThings);
+    event.preventDefault();
+  });
+
 });
